c91b57bc742e9d22c13a967cbfeb27b79edcf88f
upparamani <upparamani@gmail.com>
2016-07-30 00:15:46 +0530

    Add files via upload

---
+++ Union_Intersection_Arrays.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package union_intersection_arrays;
+import java.util.Scanner;
+/**
+ *
+ * @author 1
+ */
+public class Union_Intersection_Arrays {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         int m, n, k, i = 0, flag = 0;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter no. of elements you want in first array:");
+        m = s.nextInt();
+        System.out.print("Enter no. of elements you want in second array:");
+        n = s.nextInt();
+        k = m;
+        int a[] = new int[m];
+        int b[] = new int[n];
+        int c[] = new int[m+n];
+        int d[] = new int[m];
+        System.out.println("Enter all the elements in first array:");
+        for(i = 0; i < m; i++)
+        {
+            a[i] = s.nextInt();
+        }
+        System.out.println("Enter all the elements in second array:");
+        for(i = 0; i < n; i++)
+        {
+            b[i] = s.nextInt();
+        }
+        for(i = 0; i < m; i++)
+        {
+            c[i] = a[i];
+        }
+        for(i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                if(b[i] != a[j])
+                {
+                    flag = 1;
+                }
+                else
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+            if(flag == 1)
+            {
+                c[k] = b[i];
+                k++;
+            }
+        }
+        System.out.print("Union:");
+        for(i = 0; i < k - 1; i++)
+        {
+            System.out.print(c[i]+",");
+        }
+        System.out.print(c[k-1]);
+        System.out.println("");
+        int p = 0;
+        for(i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                if(b[i] == a[j])
+                {
+                    flag = 1;
+                    break;
+                }
+                else
+                {
+                    flag = 0;
+                }
+            }
+            if(flag == 1)
+            {
+                d[p] = b[i];
+                p++;
+            }
+        }
+        System.out.print("Intersection:");
+        if(p > 1)
+        {
+            for(i = 0; i < p - 1; i++)
+            {
+                System.out.print(d[i]+",");
+            }
+            System.out.print(d[p-1]);
+        }
+        else
+        {
+            System.out.println("Null set");
+        }
+    }            
+    }
+    
